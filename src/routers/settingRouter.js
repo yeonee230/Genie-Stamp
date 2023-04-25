0a001d37e547ad91641c3eb79ad4847fbce1b5a5
SUYEON KIM <yeonee230@gmail.com>
2023-04-26 01:48:33 +0900

    update comments

--- src/routers/settingRouter.js
+++ src/routers/settingRouter.js
@@ -4,10 +4,9 @@ import { getSetting } from "../controllers/userController";
 
 const settingtRouter = express.Router();
 
-settingtRouter.get("/", getSetting);
-settingtRouter.post("/stamp/add", addStampType);
-settingtRouter.get("/stamp/:id([0-9a-f]{24})/delete", deleteStampType);
-// studentRouter.get("/delete", deleteStamp);
-// studentRouter.get("/edit", editStamp);
+settingtRouter.get("/", getSetting);//학생 및 도장 관리
+settingtRouter.post("/stamp/add", addStampType);//도장종류 추가
+//settingtRouter.get("/stamp/:id([0-9a-f]{24})/delete", deleteStampType);//도장종류 삭제
+// studentRouter.get("/stamp/:id([0-9a-f]{24})/edit", editStamp);
 
 export default settingtRouter;
--- src/routers/studentRouter.js
+++ src/routers/studentRouter.js
@@ -12,13 +12,10 @@ import {
 
 const studentRouter = express.Router();
 
-//studentRouter.get("/", getStudents);
 studentRouter.post("/add", postAddStudent); //학생추가
-// studentRouter.get("/add", addStamp);
 studentRouter.get("/:id([0-9a-f]{24})/delete", delStudent);//학생 삭제
-// studentRouter.get("/edit", editStamp);
 studentRouter.post("/:id([0-9a-f]{24})/update", updateStampValue2); //학생 개별 도장 개수 업데이트
-studentRouter.route("/password-change").get(getPwChange).post(postPwChange); //학생 개별 도장 개수 업데이트
+studentRouter.route("/password-change").get(getPwChange).post(postPwChange); //학생 비밀번호 변경
 studentRouter.get("/:id([0-9a-f]{24})/reset-password", resetPW);//학생비밀번호 리셋
 
 export default studentRouter;
